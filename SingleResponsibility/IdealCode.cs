03f2a9561933a1ad95cd744e96359597ec487f8a
berkayzaimdev <berkayzaimdev@gmail.com>
2023-09-14 17:13:54 +0300

    Added: Single Responsibility Principle

---
+++ SingleResponsibility/IdealCode.cs
@@ -0,0 +1,36 @@
+﻿using SingleResponsibility;
+
+namespace SingleResponsibility_IdealCode
+{
+    class Database
+    {
+        public void Connect()
+        {
+            // işlemler..
+            Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+        }
+
+        public void Disconnect()
+        {
+            // işlemler..
+            Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+        }
+
+        public string State { get; set; }
+    }
+
+    class PersonService 
+        // Tek Sorumluluk Prensibine uyması için Person ile ilgili olan metodu, PersonService adında yeni bir sınıf tanımlayarak Database sınıfından ayırdık.
+        // Böylece GetPersons() metodunu değiştirmemiz gereken durumlarda tüm Database sınıfı yerine sadece PersonService sınıfını değiştirerek sorunu çözebileceğiz.
+    {
+        public List<Person> GetPersons()
+        {
+            return new()
+            {
+                new(){Name="Hilmi", Surname="Celayir"},
+                new(){Name="Mustafa", Surname="Yıldız"},
+                new(){Name="Cafer", Surname="Muiddinoğlu"}
+            };
+        }
+    }
+}
---
+++ SingleResponsibility/NotIdealCode.cs
@@ -0,0 +1,40 @@
+﻿using SingleResponsibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility_NotIdealCode
+{
+    class Database
+    {
+        public void Connect() 
+        {
+            // işlemler..
+            Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+        }
+
+        public void Disconnect() 
+        {
+            // işlemler..
+            Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+        }
+
+        public string State { get; set; }
+
+        public List<Person> GetPersons()
+            // Burada GetPersons metodunun çağrılması, yani Person ile ilgili bir işlem yapılması Single Responsibility prensibine aykırıdır. Test ve bakım maliyetlerini arttırır.
+            // Bir nesne, sadece bir nesneye karşı sorumlu olmalıdır. Bu prensibi uygulamak hem test işlemlerinde, hem de projenin yönetilmesinde yararlı olacaktır.
+            // Projede bir nesnenin değiştirilmesi gerektiğinde tüm metotlarını baştan yazmak yerine sadece etkilediği durumu ilgilendiren metotların baştan yazılması zaman açısından verimlilik sağlar.
+            // Bu yüzden bir yapı, sadece tek bir işi üstlenmelidir.
+        {
+            return new()
+            {
+                new(){Name="Hilmi", Surname="Celayir"},
+                new(){Name="Mustafa", Surname="Yıldız"},
+                new(){Name="Cafer", Surname="Muiddinoğlu"}
+            };
+        }
+    }
+}
