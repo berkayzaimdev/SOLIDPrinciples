dc2dd1ca7f7fd0e78a4fe81a72cce1e1926adbc4
berkayzaimdev <berkayzaimdev@gmail.com>
2023-09-15 12:45:34 +0300

    Added: Dependency Inversion Principle

---
+++ DependencyInversion/IdealCode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion.Ideal
+{
+    //Mercedes ve Ferrari markalı otomobil sınıflarında; IAutomobile arayüzü kullanarak AutomobileService sınıfının constructor’ ında enjekte ettik
+    //ve bu şekilde geliştirmemizi prensibimize uygun hale getirmiş olduk.
+    public interface IAutomobile
+    {
+        public void Run();
+        public void Stop();
+    }
+
+    public class Ferrari : IAutomobile
+    {
+        public void Run()
+        {
+            Console.WriteLine("Ferrari çalıştırıldı");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Ferrari durduruldu.");
+        }
+    }
+
+    public class Mercedes : IAutomobile
+    {
+        public void Run()
+        {
+            Console.WriteLine("Mercedes çalıştırıldı");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Mercedes durduruldu.");
+        }
+    }
+
+    public class AutomobileService
+    {
+        private readonly IAutomobile automobile;
+
+        public AutomobileService(IAutomobile automobile)
+        {
+            this.automobile = automobile;
+        }
+
+        public void Start()
+        {
+            automobile.Run();
+        }
+
+        public void Stop()
+        {
+            automobile.Stop();
+        }
+    }
+}
---
+++ DependencyInversion/NotIdealCode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion.NotIdeal
+{
+    // Buradaki temel problem, yüksek seviyeli sınıfın düşük seviyeli sınıflara bağımlı olmasıdır.
+    // Dependency Inversion Prensibi, bu soruna "Yüksek seviyeli sınıflar, düşük seviyeli sınıflara bağlı olmamalı, her ikisi de soyut kavramlara bağlı olmalıdır" görüşü ile çözüm getirmektedir.
+    // Sınıfları ayrık bir şekilde tanımlamak yerine soyutlama yardımıyla ortak noktalarını bir kümeye alabiliriz. Bu kümeyi Interface yardımıyla oluşturabiliriz.
+
+    // Soyutlamalar ayrıntılara bağlı olmamalıdır. Detaylar soyutlamalara bağlı olmalıdır.
+    // Yani alt sınıflarda yapılan bir değişiklik üst sınıfları etkilememelidir.
+
+    public class Ferrari
+    {
+        public void Run()
+        {
+            Console.WriteLine("Ferrari çalıştırıldı");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Ferrari durduruldu.");
+        }
+    }
+
+    public class Mercedes
+    {
+        public void Run()
+        {
+            Console.WriteLine("Mercedes çalıştırıldı");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Mercedes durduruldu.");
+        }
+    }
+
+    public class AutomobileService
+    {
+        public void Run()
+        {
+            var ferrari = new Ferrari();
+            ferrari.Run();
+
+            var mercedes = new Mercedes();
+            mercedes.Run();
+        }
+
+        public void Stop()
+        {
+            var ferrari = new Ferrari();
+            ferrari.Stop();
+
+            var mercedes = new Mercedes();
+            mercedes.Stop();
+        }
+    }
+}
