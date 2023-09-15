dabff54080b7a2e09a4f5ebb967df102a8d34bc4
berkayzaimdev <berkayzaimdev@gmail.com>
2023-09-15 11:45:33 +0300

    Added: Open-Closed Principle

---
+++ OpenClosed/IdealCode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed.Ideal
+{
+    class ParaGonderici
+    {
+        public void Gonder(IBanka banka, int tutar, string hesapNo)
+        {
+            banka.ParaTransferi(tutar, hesapNo);
+        }
+    }
+
+    interface IBanka
+        // Tüm bankaların ortak bir Interface'ten kalıtım alır. Bu şekilde her bankanın ParaTransferi metodunu kullanması zorunlu hale getirilir.
+        // Bu prensip koda Sürdürülebilirlik, Genişletilebilirlik, Yeniden kullanılabilirlik, Esneklik kazandırır.
+    {
+        bool ParaTransferi(int tutar, string hesapNo);
+    }
+
+    class Albaraka : IBanka
+    {
+        public string HesapNo { get; set; }
+        public void ParaGonder(int tutar)
+        {
+            //...
+        }
+
+        public bool ParaTransferi(int tutar, string hesapNo)
+        {
+            try
+            {
+
+                HesapNo = hesapNo;
+                ParaGonder(tutar);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+    class Akbank : IBanka
+    {
+        string _hesapNo;
+        public void GonderilecekHesapNo(string hesapNo)
+        {
+            //...
+        }
+        public void Gonder(int tutar)
+        {
+            //...
+        }
+
+        public bool ParaTransferi(int tutar, string hesapNo)
+        {
+            try
+            {
+                GonderilecekHesapNo(hesapNo);
+                Gonder(tutar);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+    class BerkayBank : IBanka
+    {
+        public bool ParaTransferi(int tutar, string hesapNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenClosed/NotIdealCode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed.NotIdeal
+{
+    class ParaGonderici
+    {
+        public void Gonder(int tutar)
+        {
+            //Akbank akbank = new();
+            //akbank.HesapNo = "...";
+            //akbank.ParaGonder(tutar);
+            Albaraka albaraka = new();
+            albaraka.GonderilecekHesapNo("123");
+            albaraka.Gonder(tutar);
+        }
+    }
+
+    // Para gönderme metodunu değiştirmek istediğimizde, mesela ekleme esnasında doğrulama için CAPTCHA eklemek istediğimizde tüm metotları değiştirmemiz gerekir.
+    // Bu durum hem gereksiz kod kalabalığı, hem de maliyet ve verimlilik açısından zarar oluşturacaktır.
+    // Bu nedenle tüm bankaların ortak kalıtım aldığı bir yapı, ve o yapıdan aldığı ortak metot oluşturmalıyız.
+    // Bu yapı da Interface'e karşılık geliyor.
+
+    class Albaraka
+    {
+        string _hesapNo;
+        public void GonderilecekHesapNo(string hesapNo)
+        {
+            //...
+        }
+        public void Gonder(int tutar)
+        {
+            //...
+        }
+    }
+
+    class Akbank
+    {
+        public string HesapNo { get; set; }
+        public void ParaGonder(int tutar)
+        {
+            //...
+        }
+    }
+}
