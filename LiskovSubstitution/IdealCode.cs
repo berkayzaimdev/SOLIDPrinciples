6f7694b44999e22e7044b37332b0c1c5203bef57
berkayzaimdev <berkayzaimdev@gmail.com>
2023-09-15 12:00:03 +0300

    Added: Liskov Substitution Principle

---
+++ LiskovSubstitution/IdealCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    abstract class Cloud
+    {
+        public abstract void MachineLearning();
+    }
+    // MachineLearning() metodu bütün Child sınıflarda kullanıldığı için, hepsinde ortak olan Cloud sınıfını oluşturduk.
+
+    interface ITranslatable
+    {
+        void Translate();
+    }
+    // Translate() metodu, daha az sayıda Child sınıfta kullanıldığı için, bunu bir Interface'de asıl sıınıfa haricen dahil ettik.
+
+    // Bu prensibe gerçek hayattan örnek verecek olursak, hayvanlar aleminden örnek verebiliriz.
+    // Kedi yürüyebilir. Timsah hem yürüyebilir, hem de yüzebilir. Ördek hem yürüyebilir, hem yüzebilir, hem de uçabilir.
+    // Bu bağlamda bütün hareket şekillerini bir sınıfta tanımlamak ve override etmek yerine, ana bir sınıfta hepsinde ortak olan yürüme hareketini tanımlayıp, yüzme ve uçmayı ise harici bir Interface'te tanımlayabiliriz.
+
+    class AWS : Cloud, ITranslatable
+    {
+        public void Translate()
+           => Console.WriteLine("AWS Translate");
+        override public void MachineLearning()
+            => Console.WriteLine("AWS Machine Learning");
+    }
+
+    class Azure : Cloud
+    {
+        override public void MachineLearning()
+            => Console.WriteLine("Azure Machine Learning");
+    }
+
+    class Google : Cloud, ITranslatable
+    {
+        public void Translate()
+           => Console.WriteLine("Google Translate");
+
+        override public void MachineLearning()
+            => Console.WriteLine("Google Machine Learning");
+    }
+}
---
+++ LiskovSubstitution/NotIdealCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution.NotIdeal
+{
+    abstract class Cloud
+    {
+        public abstract void Translate(); // Bu metot tüm sınıflarda sağlanmamaktadır
+        public abstract void MachineLearning(); // Bu metot tüm sınıflarda sağlanmaktadır
+    }
+
+    // Bu prensipteki ana düşüncemiz; eğer kalıtım alınan bir sınıf veya Interface varsa, bu Parent nesnedeki tüm metotlar Child nesnelerde kullanılmalıdır.
+    // Eğer o metotları boş bırakırsak yahut hata fırlatırsak program bir noktada sıkışacak, hata verecektir.
+
+    class AWS : Cloud
+    {
+        override public void Translate()
+            => Console.WriteLine("AWS Translate");
+        override public void MachineLearning()
+            => Console.WriteLine("AWS Machine Learning");
+    }
+
+    class Azure : Cloud
+    {
+        override public void Translate()
+            => throw new NotImplementedException();
+
+        override public void MachineLearning()
+            => Console.WriteLine("Azure Machine Learning");
+    }
+
+    class Google : Cloud
+    {
+        override public void Translate()
+            => Console.WriteLine("Google Translate");
+
+        override public void MachineLearning()
+            => Console.WriteLine("Google Machine Learning");
+    }
+}
